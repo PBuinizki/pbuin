cf45b8b465c419372e1d22eaf6dfcd6b77543754
PBuinizki <61580868+PBuinizki@users.noreply.github.com>
2023-01-19 01:00:06 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
